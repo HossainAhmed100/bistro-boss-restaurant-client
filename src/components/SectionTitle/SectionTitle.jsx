afc5c09c924a671d522fc764fbbab7977a08a5c5
HossainAhmed100 <successfulllife101@gmail.com>
2024-03-02 07:24:29 +0600

    create popularmenu section

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -4,9 +4,9 @@ const SectionTitle = ({heading, subHeading}) => {
   return (
     <div className="max-w-md text-center">
       <p className="text-orange-400 text-lg">{subHeading}</p>
-      <Divider className="my-4" />
+      <Divider className="my-4 h-1" />
       <h3 className="text-5xl font-medium uppercase">{heading}</h3>
-      <Divider className="my-4" />
+      <Divider className="my-4 h-1" />
     </div>
   )
 }
---
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,11 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle"
+
+const PopularMenu = () => {
+  return (
+    <section>
+        <SectionTitle subHeading={"---Check it out---"} heading={"FROM OUR MENU"}/>
+    </section>
+  )
+}
+
+export default PopularMenu
